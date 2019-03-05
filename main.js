51df1f3ee862e155a5a808685afaa1bfc70e4806
Silvestre Luna <slvtrluna@gmail.com>
2019-03-04 19:55:37 -0600

    adding if statement

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("Hello");
+//bonus = print to DOM
+const PrintToDOM = (divId, textToPrint ) => {
+    const selectDiv = document.getElementById(divId);
+    selectDiv.innerHTML += textToPrint;
+};
+
+const iAmThursty = (num) => {
+    /// do something 
+    //is under 21 "dring some water"
+    if (num < 21) {
+        return 'drink some water!';
+    //ove 21, under 65 "have a beer"
+    } else if ( num >= 21 && num < 65 ) {
+        return 'have a beer';
+    } 
+        // over 65 'take a nap'
+    else {
+        return 'Take a nap!';
+    }
+};
+
+//bonus = print to DOM
+
+PrintToDOM('drink', iAmThursty(55));
+PrintToDOM('drink', iAmThursty(11));
+PrintToDOM('drink', iAmThursty(67));
+PrintToDOM('drink', iAmThursty(3));
+PrintToDOM('drink', iAmThursty(-23));
+
+
+
